6d721536ee08ff5645c1fd9dac48d9202d09362b
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-02-02 21:07:17 -0300

    Criar banco de dados, ok.

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
+# -*- coding: utf-8 -*-
 import mysql.connector
+
+
+
+
 
 mydb = mysql.connector.connect(
 	host="localhost",
-	database="otakumus",
 	user="root",
 	password="Zebobin10@"
 )
 
 
-mycursor = mydb.cursor()
+if mydb.is_connected():
+	db_info = mydb.get_server_info()
+  	print("Conectado ao banco de dados: ",db_info)
+	cursor = mydb.cursor()
+	cursor.execute("CREATE DATABASE OlaMundo")
+	linha = cursor.fetchone()
+	print("Conectado ao banco de dados", linha)
+
+
+
+
+
+
+
+
+
+
+if mydb.is_connected():
+	cursor.close()
+	mydb.close()
+	print("Encerrada")
+
+
+
 
 	
 
